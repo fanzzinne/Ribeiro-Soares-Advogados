6068e8cb3e669990dbea142574e9037f0585580c
fanzzinne <fanzzinne@gmail.com>
2026-07-13 06:30:38 -0300

    commit

--- sw.js
+++ sw.js
@@ -1,13 +1,37 @@
-const CACHE_NAME = 'rs-luxury-v1';
+const CACHE_NAME = 'rs-advogados-v1';
 const ASSETS = [
+  '/',
   'index.html',
-  'manifest.json'
+  'sobre.html',
+  'especialidades.html',
+  'socios.html',
+  'manifest.json',
+  'assets/css/style.css',
+  'assets/img/logo.png.png',
+  'assets/img/Escritorio.png',
+  'assets/img/Natália Soares.png',
+  'assets/img/Agatha Ribeiro.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
